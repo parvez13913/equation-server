076eb012490d8c964cc764f1bb4b509fed1c8980
parvez13913 <parvezz13913@gmail.com>
2022-06-24 00:57:22 -0700

    equationData api is complite

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ia7ky.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run() {
+    try {
+        await client.connect();
+        const equationDataCollection = client.db('equationProject').collection('equationData');
+
+        app.get('/equationData', async (req, res) => {
+            const quary = {};
+            const cursor = equationDataCollection.find(quary);
+            const alphabet = await cursor.toArray();
+            res.send(alphabet);
+        });
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', async (req, res) => {
     res.send("Hello");
